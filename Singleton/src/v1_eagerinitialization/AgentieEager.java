237398a675e9f066ee866e6d35688fc6dc092933
romancostin14 <romancostin14@stud.ase.ro>
2025-03-26 15:56:51 +0200

    Commit for Singleton example

---
+++ Singleton/src/v1_eagerinitialization/AgentieEager.java
@@ -0,0 +1,44 @@
+package v1_eagerinitialization;
+
+public class AgentieEager {
+    private String numeAgentie;
+    private float capital;
+    private int numarAngajati;
+
+    private static final AgentieEager instanta = new AgentieEager();
+
+    private AgentieEager() {
+        this.numeAgentie = "AgeTur";
+        this.capital = 1000;
+        this.numarAngajati = 5;
+    }
+
+    public String getNumeAgentie() {
+        return numeAgentie;
+    }
+
+    public float getCapital() {
+        return capital;
+    }
+
+    public int getNumarAngajati() {
+        return numarAngajati;
+    }
+
+    public void setNumeAgentie(String numeAgentie) {
+        this.numeAgentie = numeAgentie;
+    }
+
+    public void setCapital(float capital) {
+        this.capital = capital;
+    }
+
+    public void setNumarAngajati(int numarAngajati) {
+        this.numarAngajati = numarAngajati;
+    }
+
+    public static AgentieEager getInstance(String nume, float capital, int numarAngajati) {
+        return instanta;
+    }
+
+}
